c9458f3d73530f9871b5687449850733b24bf7be
cunros <79799304+cunros@users.noreply.github.com>
2025-07-23 02:04:43 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAEhPhjnPeZR3a9gP_9Hrl_tbZ5lOkCqew",
   authDomain: "snakegame-fa0d4.firebaseapp.com",
   projectId: "snakegame-fa0d4",
-  storageBucket: "snakegame-fa0d4.firebasestorage.app",
+  storageBucket: "snakegame-fa0d4.appspot.com",
   messagingSenderId: "784708945540",
   appId: "1:784708945540:web:2c672219ee458aa51ee4d1"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
