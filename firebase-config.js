b55cb582caea54668af0645670401b93a6050290
cunros <79799304+cunros@users.noreply.github.com>
2025-07-23 02:32:25 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyAEhPhjnPeZR3a9gP_9Hrl_tbZ5lOkCqew",
   authDomain: "snakegame-fa0d4.firebaseapp.com",
@@ -10,5 +7,6 @@ const firebaseConfig = {
   appId: "1:784708945540:web:2c672219ee458aa51ee4d1"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
